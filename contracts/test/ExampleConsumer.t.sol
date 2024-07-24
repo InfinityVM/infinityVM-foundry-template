9334503f92c367acdeb8051fc4def7d06c595865
Maanav Khaitan <maanav@berkeley.edu>
2024-07-24 15:50:33 -0400

    Cleaned up tests

--- contracts/test/ExampleConsumer.t.sol
+++ contracts/test/ExampleConsumer.t.sol
@@ -13,16 +13,11 @@ contract CoprocessorTest is Test, Deployer {
     address RELAYER = address(1);
     address COPROCESSOR_OPERATOR = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
-    event JobCreated(uint32 indexed jobID, uint64 maxCycles, bytes32 indexed programID, bytes programInput);
-    event JobCompleted(uint32 indexed jobID, bytes result);
-
     function setUp() public {
         deployContracts(RELAYER, COPROCESSOR_OPERATOR);
     }
 
     function test_Consumer_RequestJob() public {
-        vm.expectEmit(true, true, true, true);
-        emit JobCreated(1, DEFAULT_MAX_CYCLES, ImageID.ADDRESS_BALANCE_ID, abi.encode(address(2)));
         uint32 jobID = consumer.requestBalance(address(2));
         assertEq(jobID, 1);
         assertEq(consumer.getProgramInputsForJob(jobID), abi.encode(address(2)));
