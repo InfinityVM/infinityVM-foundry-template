3f1b9ac7d0c4766abafa0cb4b8751773a6ca7152
Zeke Mostov <zeke@turnkey.io>
2024-08-14 15:03:34 -0400

    add withdraw

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,9 +1,6 @@
 use axum::{extract::State as ExtractState, Json};
-use clob_core::api::{DepositRequest, DepositResponse, Request};
-use tokio::sync::{
-    mpsc::Sender,
-    oneshot,
-};
+use clob_core::api::{DepositRequest, DepositResponse, Request, WithdrawRequest, WithdrawResponse};
+use tokio::sync::{mpsc::Sender, oneshot};
 
 pub mod engine;
 
@@ -15,7 +12,7 @@ pub struct ServerState {
 pub async fn http_listen(state: ServerState, listen_address: &str) {
     let app = axum::Router::new()
         .route("/deposit", axum::routing::post(deposit))
-        // .route("/withdraw", axum::routing::post(withdraw))
+        .route("/withdraw", axum::routing::post(withdraw))
         // .route("/orders", axum::routing::post(place_order))
         // .route("/cancel", axum::routing::post(cancel))
         // .route("/status", axum::routing::post(order_status))
@@ -35,8 +32,22 @@ async fn deposit(
     sender.send((Request::Deposit(req), tx)).await.expect("todo");
     let global_index = rx.await.expect("todo");
     println!("deposit: global_index: {:?}", global_index);
-
     // TODO if we want preconfs sign global index and put in response
 
     Json(DepositResponse { success: true })
+}
+
+async fn withdraw(
+    ExtractState(state): ExtractState<ServerState>,
+    Json(req): Json<WithdrawRequest>,
+) -> Json<WithdrawResponse> {
+    let sender = state.engine_sender;
+    let (tx, rx) = oneshot::channel::<u64>();
+
+    sender.send((Request::Withdraw(req), tx)).await.expect("todo");
+    let global_index = rx.await.expect("todo");
+    println!("withdraw: global_index: {:?}", global_index);
+    // TODO if we want preconfs sign global index and put in response
+
+    Json(WithdrawResponse { success: true })
 }
