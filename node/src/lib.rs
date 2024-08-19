e0abb7ca25cfced30994bc6c98843125052c7136
Zeke Mostov <zeke@turnkey.io>
2024-08-18 22:12:20 -0400

    add test setup

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -21,22 +21,38 @@ use tokio::sync::{mpsc::Sender, oneshot};
 pub mod db;
 pub mod engine;
 
-/// Stateful parts of rest server
+const DEPOSIT: &str = "/deposit";
+const WITHDRAW: &str = "/withdraw";
+const ORDERS: &str = "/orders";
+const CANCEL: &str = "/cancel";
+const CLOB_STATE: &str = "/clob-state";
+
+/// Stateful parts of REST server
 #[derive(Debug, Clone)]
 pub struct ServerState {
     /// Engine send channel handle.
-    pub engine_sender: Sender<(Request, oneshot::Sender<ApiResponse>)>,
+    engine_sender: Sender<(Request, oneshot::Sender<ApiResponse>)>,
     /// The database
-    pub db: Arc<DatabaseEnv>,
+    db: Arc<DatabaseEnv>,
+}
+
+impl ServerState {
+    /// Create a new instance of [Self].
+    pub fn new(
+        engine_sender: Sender<(Request, oneshot::Sender<ApiResponse>)>,
+        db: Arc<DatabaseEnv>,
+    ) -> Self {
+        Self { engine_sender, db }
+    }
 }
 
 fn app(state: ServerState) -> Router {
     axum::Router::new()
-        .route("/deposit", axum::routing::post(deposit))
-        .route("/withdraw", axum::routing::post(withdraw))
-        .route("/orders", axum::routing::post(add_order))
-        .route("/cancel", axum::routing::post(cancel))
-        .route("/clob-state", axum::routing::get(clob_state))
+        .route(DEPOSIT, axum::routing::post(deposit))
+        .route(WITHDRAW, axum::routing::post(withdraw))
+        .route(ORDERS, axum::routing::post(add_order))
+        .route(CANCEL, axum::routing::post(cancel))
+        .route(CLOB_STATE, axum::routing::get(clob_state))
         .with_state(state)
 }
 
@@ -121,6 +137,75 @@ async fn clob_state(ExtractState(state): ExtractState<ServerState>) -> Json<Clob
     Json(clob_state)
 }
 
-// ref for testing: https://github.com/tokio-rs/axum/blob/main/examples/testing/src/main.rs
 #[cfg(test)]
-mod tests {}
+mod tests {
+    // ref for testing: https://github.com/tokio-rs/axum/blob/main/examples/testing/src/main.rs
+    use super::*;
+    use axum::{
+        body::Body,
+        extract::connect_info::MockConnectInfo,
+        http::{self, Request as AxumRequest, StatusCode},
+    };
+    use clob_core::api::{DepositResponse, UserBalance};
+    use http_body_util::BodyExt;
+    use std::{fs::File, io::Write};
+    use tempfile::tempdir;
+    use tokio::task::JoinHandle;
+    use tower::{Service, ServiceExt};
+
+    const CHANEL_SIZE: usize = 32;
+
+    // Simple wrapper for tokio task handle to make sure it aborts
+    struct DropAbort<T>(JoinHandle<T>);
+    impl<T> Drop for DropAbort<T> {
+        fn drop(&mut self) {
+            self.0.abort_handle().abort();
+        }
+    }
+
+    async fn test_setup() -> (ServerState, DropAbort<()>) {
+        let dbdir = tempdir().unwrap();
+        let db = Arc::new(crate::db::init_db(dbdir).unwrap());
+        let (engine_sender, engine_receiver) = tokio::sync::mpsc::channel(CHANEL_SIZE);
+
+        let server_state = ServerState::new(engine_sender, Arc::clone(&db));
+
+        let engine_handle =
+            tokio::spawn(async move { crate::engine::run_engine(engine_receiver, db).await });
+
+        (server_state, DropAbort(engine_handle))
+    }
+
+    async fn post<Req, Resp>(app: &mut Router, uri: &str, req: Req) -> Resp
+    where
+        Req: serde::Serialize,
+        Resp: serde::de::DeserializeOwned,
+    {
+        let body = Body::from(serde_json::to_vec(&req).unwrap());
+
+        let request = AxumRequest::builder()
+            .uri(uri)
+            .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .method(http::Method::POST)
+            .body(body)
+            .unwrap();
+
+        let response =
+            ServiceExt::<AxumRequest<Body>>::ready(app).await.unwrap().call(request).await.unwrap();
+        let bytes = response.into_body().collect().await.unwrap().to_bytes();
+        serde_json::from_slice(&bytes).unwrap()
+    }
+
+    #[tokio::test]
+    async fn place_bids() {
+        let (server_state, _) = test_setup().await;
+        let mut app = app(server_state);
+
+        let response: ApiResponse = post(
+            &mut app,
+            DEPOSIT,
+            DepositRequest { address: [0; 20], amounts: UserBalance { a: 10, b: 10 } },
+        )
+        .await;
+    }
+}
